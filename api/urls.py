ea1a84c87c77c30cb428f6e619724129c5d0db7f
Jahir <Jahir@varianceinfotech.com>
2022-09-28 12:44:00 +0530

    urls.py changed

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import TaskAPIView, TodoAPIView, BulkTodoAPIView
 
 urlpatterns = [
     path('todo-nested/', TodoAPIView.as_view(), name='todo-api-view'),
-    path('todo-nested/', TodoAPIView.as_view(), name='todo-api-view'),
+    path('task-nested/', TaskAPIView.as_view(), name='task-api-view'),
     path('bulk-todo-nested/', BulkTodoAPIView.as_view(), name='bulk-todo-api-view'),
 ]
